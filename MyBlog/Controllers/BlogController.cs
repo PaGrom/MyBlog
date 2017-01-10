d6e563ba36fdd1ec256f73f57d3ec6dcdfad4cba
Pavel Gromov <pagrom23@gmail.com>
2017-01-11 01:41:24 +0300

    Add Controller and Model

---
+++ MyBlog/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using MyBlog.Core;
+using MyBlog.Models;
+
+namespace MyBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public ViewResult Posts(int pageNumber = 1)
+        {
+            var viewModel = new ListViewModel(_blogRepository, pageNumber);
+
+            ViewBag.Title = "Latest Posts";
+
+            return View("List", viewModel);
+        }
+    }
+}
---
+++ MyBlog/Models/ListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyBlog.Core.Objects;
+using MyBlog.Core;
+
+namespace MyBlog.Models
+{
+    public class ListViewModel
+    {
+        public ListViewModel(IBlogRepository _blogRepository, int pageNumber)
+        {
+            Posts = _blogRepository.Posts(pageNumber - 1, 10);
+            TotalPosts = _blogRepository.TotalPosts();
+        }
+
+        public IList<Post> Posts { get; private set; }
+        public int TotalPosts { get; private set; }
+    }
+}
