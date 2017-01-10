28b2048b654108b4cf090e1f71c88f82dca602ba
Pavel Gromov <pgromov@acumatica.com>
2017-01-11 00:36:23 +0300

    Start to write

---
+++ MyBlog.Core/IBlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlog.Core.Objects;
+
+namespace MyBlog.Core
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int pageNo, int pageSize);
+        int TotalPosts();
+    }
+}
