a0223d0cfcefd54d0e290af9ee61fc9e760ab945
Pavel Gromov <pagrom23@gmail.com>
2017-01-12 02:10:16 +0300

    Add views

---
+++ MyBlog/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using MyBlog.Core.Objects;
+
+namespace MyBlog
+{
+    public static class Extensions
+    {
+        public static string ToConfigLocalTime(this DateTime utcDT)
+        {
+            var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+            return string.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        }
+    }
+}
